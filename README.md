a8854a2d336de2c9e49b41ed320696253f6e18e0
jsaint73 <95711374+jsaint73@users.noreply.github.com>
2026-09-12 18:59:23 -0400

    All the files from Scrimba
    Finished first scrimba project

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# BasketBall-Scoreboard
- This is primarily a                 JavaScript project where we are to use the knowledge gained within module 3 of Scrimba's frontend career path course, we                 will be doing the HTML &amp; CSS as well.
+# Getting Started
+Install the dependencies and run the project
+```
+npm install
+npm start
+```
+
+Head over to https://vitejs.dev/ to learn more about configuring vite
+## About Scrimba
+
+At Scrimba our goal is to create the best possible coding school at the cost of a gym membership! 💜
+If we succeed with this, it will give anyone who wants to become a software developer a realistic shot at succeeding, regardless of where they live and the size of their wallets 🎉
+The Fullstack Developer Path aims to teach you everything you need to become a Junior Developer, or you could go further with one of our advanced courses 🚀
+
+- [Our courses](https://scrimba.com/courses)
+- [The Frontend Career Path](https://scrimba.com/fullstack-path-c0fullstack)
+- [Become a Scrimba Pro member](https://scrimba.com/pricing)
+
+Happy Coding!
---
+++ index.js
@@ -0,0 +1,44 @@
+let homeScoreBtnOne = document.getElementById("home-score-btn-1")
+let homeScoreBtnTwo = document.getElementById("home-score-btn-2")
+let homeScoreBtnThree = document.getElementById("home-score-btn-3")
+
+let awayScoreBtnOne = document.getElementById("away-score-btn-1")
+let awayScoreBtnTwo = document.getElementById("away-score-btn-2")
+let awayScoreBtnThree = document.getElementById("away-score-btn-3")
+
+let homeStoreEl = document.getElementById("home-score")
+let homeScore = 0
+
+let awayStoreEl = document.getElementById("away-score")
+let awayScore = 0
+
+function increaseHomeScoreOne(){
+    homeScore += 1
+    homeStoreEl.textContent = homeScore
+}
+
+function increaseHomeScoreTwo(){
+    homeScore += 2
+    homeStoreEl.textContent = homeScore
+}
+
+function increaseHomeScoreThree(){
+    homeScore += 3
+    homeStoreEl.textContent = homeScore
+}
+
+
+function increaseAwayScoreOne(){
+    awayScore += 1
+    awayStoreEl.textContent = awayScore
+}
+
+function increaseAwayScoreTwo(){
+    awayScore += 2
+    awayStoreEl.textContent = awayScore
+}
+
+function increaseAwayScoreThree(){
+    awayScore += 3
+    awayStoreEl.textContent = awayScore
+}
